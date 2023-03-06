93d1407365934db1580c38d4c0b2ffc825561457
Sina Sharifi <103168440+SinAshariffi@users.noreply.github.com>
2023-03-06 15:32:13 +0330

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -31,7 +31,7 @@ try {
   
   
   // adding fire account for paying the gas fee
-  const PHRASE = 'negative cheap cherry uncover absurd angle swarm armor tuna lounge hurdle lawsuit';
+  const PHRASE = 'gaze clutch achieve nice direct praise noodle resist blind amazing pact clown';
   const seedUser = mnemonicToMiniSecret(PHRASE);
   const keyring = new Keyring({ type: "ed25519" });
   const userKeyring = keyring.addFromPair(naclKeypairFromSeed(seedUser));
